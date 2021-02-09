7d21c276c7076b2f997872c8e1f870b145ffda96
kais <xup6u06m4@gmail.com>
2021-02-09 16:35:00 +0800

    add 'get account list', 'get account detail' api

--- src/backend/libs/Account.js
+++ src/backend/libs/Account.js
@@ -112,6 +112,129 @@ class Account extends Bot {
       return new ResponseFormat({ message: `DB Error(${e.message})`, code: Codes.DB_ERROR });
     }
   }
+
+  async AccountList({ token }) {
+    if (!token) return new ResponseFormat({ message: 'invalid token', code: Codes.INVALID_ACCESS_TOKEN });
+    const tokenInfo = await Utils.verifyToken(token);
+    const payload = [];
+
+    try {
+      const findAccounts = await this.accountModel.findAll({
+        where: {
+          User_id: tokenInfo.userID,
+        },
+      });
+
+      for (let i = 0; i < findAccounts.length; i++) {
+        const account = findAccounts[i];
+        const findAccountCurrencies = await this.accountCurrencyModel.findAll({
+          where: {
+            Account_id: account.Account_id,
+          },
+          include: [
+            {
+              model: this.currencyModel,
+              attributes: ['type'],
+              where: {
+                type: 1,
+              },
+            },
+          ],
+        });
+        for (let j = 0; j < findAccountCurrencies.length; j++) {
+          const accountCurrency = findAccountCurrencies[j];
+          payload.push({
+            account_id: account.Account_id,
+            blockchain_id: account.Blockchain_id,
+            currency_id: accountCurrency.Currency_id,
+            balance: accountCurrency.balance,
+            account_index: '0',
+          });
+        }
+      }
+      return new ResponseFormat({ message: 'Get Account List', payload });
+    } catch (e) {
+      console.log(e);
+      if (e.code) return e;
+      return new ResponseFormat({ message: `DB Error(${e.message})`, code: Codes.DB_ERROR });
+    }
+  }
+
+  async AccountDetail({ token, params }) {
+    if (!token) return new ResponseFormat({ message: 'invalid token', code: Codes.INVALID_ACCESS_TOKEN });
+    const tokenInfo = await Utils.verifyToken(token);
+
+    const { currency_id } = params;
+    if (!Utils.validateString(currency_id)) return new ResponseFormat({ message: 'invalid input', code: Codes.INVALID_INPUT });
+    const payload = [];
+    const accountObj = {};
+
+    try {
+      const findAccounts = await this.accountModel.findAll({
+        where: {
+          User_id: tokenInfo.userID,
+        },
+      });
+
+      for (let i = 0; i < findAccounts.length; i++) {
+        const account = findAccounts[i];
+        const findAccountCurrencies = await this.accountCurrencyModel.findAll({
+          where: {
+            Account_id: account.Account_id,
+          },
+          include: [
+            {
+              model: this.currencyModel,
+              attributes: ['Currency_id', 'name', 'symbol', 'type', 'publish', 'decimals', 'total_supply', 'contract', 'icon'],
+              where: {
+                [this.Sequelize.Op.or]: [{ type: 1 }, { type: 2 }],
+              },
+            },
+          ],
+        });
+        for (let j = 0; j < findAccountCurrencies.length; j++) {
+          const accountCurrency = findAccountCurrencies[j];
+          if (accountCurrency.Currency && accountCurrency.Currency.type === 1) {
+            payload.push({
+              blockchain_id: account.Blockchain_id,
+              currency_id: accountCurrency.Currency_id,
+              account_id: account.Account_id,
+              purpose: account.purpose,
+              account_index: '0',
+              curve_type: account.curve_type,
+              number_of_external_key: account.number_of_external_key,
+              number_of_internal_key: account.number_of_internal_key,
+              balance: accountCurrency.balance,
+              symbol: accountCurrency.Currency.symbol,
+              icon: accountCurrency.Currency.icon,
+            });
+          } else if (accountCurrency.Currency && accountCurrency.Currency.type === 2) {
+            if (!accountObj[account.Account_id])accountObj[account.Account_id] = [];
+            accountObj[account.Account_id].push({
+              token_id: accountCurrency.Currency.Currency_id,
+              blockchain_id: account.Blockchain_id,
+              name: accountCurrency.Currency.name,
+              symbol: accountCurrency.Currency.symbol,
+              type: accountCurrency.Currency.type,
+              publish: accountCurrency.Currency.publish,
+              decimals: accountCurrency.Currency.decimals,
+              total_supply: accountCurrency.Currency.total_supply,
+              contract: accountCurrency.Currency.contract,
+              balance: accountCurrency.balance,
+            });
+          }
+        }
+      }
+      payload.forEach((item) => {
+        item.tokens = accountObj[item.account_id] || [];
+      });
+      return new ResponseFormat({ message: 'Get Account List', payload });
+    } catch (e) {
+      console.log(e);
+      if (e.code) return e;
+      return new ResponseFormat({ message: `DB Error(${e.message})`, code: Codes.DB_ERROR });
+    }
+  }
 }
 
 module.exports = Account;
