a27caaefca7e485c41c4677303d81a81c14180d6
wayne.lee <wayne.lee@tideisun.com>
2021-02-24 12:00:53 +0800

    fix some input bug

--- src/backend/libs/EthCrawlerManagerBase.js
+++ src/backend/libs/EthCrawlerManagerBase.js
@@ -10,9 +10,9 @@ const Utils = require('./Utils');
 const log_file = fs.createWriteStream(`${__dirname}/debug.log`, { flags: 'w' });
 const log_stdout = process.stdout;
 
-console.log = function (d) { //
-  log_file.write(`${util.format(d)}\n`);
-  log_stdout.write(`${util.format(d)}\n`);
+console.log = function (...d) { //
+  log_file.write(`${util.format(...d)}\n`);
+  log_stdout.write(`${util.format(...d)}\n`);
 };
 
 class EthCrawlerManagerBase extends CrawlerManagerBase {
