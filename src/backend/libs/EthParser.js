df499a7fd995ba0f52f9bec09fc8a386e5c60bc4
wayne.lee <wayne.lee@tideisun.com>
2021-02-25 15:44:59 +0800

    fix eth parser option

--- src/backend/libs/EthParser.js
+++ src/backend/libs/EthParser.js
@@ -4,7 +4,7 @@ class EthParser extends EthParserBase {
   constructor(config, database, logger) {
     super('8000003C', config, database, logger);
 
-    this.options = config.ethereum.ropsten;
+    this.options = config.ethereum;
     this.syncInterval = config.syncInterval.ethereum ? config.syncInterval.ethereum : 15000;
   }
 }
