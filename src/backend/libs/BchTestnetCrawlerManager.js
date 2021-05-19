b6ca1fa502bf79eac1c8c345459b9d0ece2a23e9
TzuHanLiang <crocodilestear@gmail.com>
2021-05-19 21:18:51 +0800

    feat: add jsonrpc info

---
+++ src/backend/libs/BchTestnetCrawlerManager.js
@@ -0,0 +1,13 @@
+const BtcCrawlerManagerBase = require('./BtcCrawlerManagerBase');
+
+class BtcTestnetCrawlerManager extends BtcCrawlerManagerBase {
+  constructor(config, database, logger) {
+    super('80000001', database, logger);
+    this.options = config.blockchain.bitcoin_testnet;
+    this.syncInterval = config.syncInterval.bitcoin_cash ? config.syncInterval.bitcoin_cash : 450000;
+    this.feeSyncInterval = config.syncInterval.fee ? config.syncInterval.fee : 3600000;
+    this.pendingTxSyncInterval = config.syncInterval.pending ? config.syncInterval.pending : 15000;
+  }
+}
+
+module.exports = BtcTestnetCrawlerManager;
