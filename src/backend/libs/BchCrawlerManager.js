bc2fbff5373f9bf305206b1b983388717a4c1bd2
TzuHanLiang <crocodilestear@gmail.com>
2021-05-20 09:09:17 +0800

    feat: add bch crawler

--- src/backend/libs/BchCrawlerManager.js
+++ src/backend/libs/BchCrawlerManager.js
@@ -1,13 +1,13 @@
-const BtcCrawlerManagerBase = require('./BtcCrawlerManagerBase');
+const BchCrawlerManagerBase = require('./BchCrawlerManagerBase');
 
-class BtcCrawlerManager extends BtcCrawlerManagerBase {
+class BchCrawlerManager extends BchCrawlerManagerBase {
   constructor(config, database, logger) {
     super('80000091', database, logger);
-    this.options = config.blockchain.bitcoin_mainnet;
+    this.options = config.blockchain.bitcoin_cash_mainnet;
     this.syncInterval = config.syncInterval.bitcoin ? config.syncInterval.bitcoin : 450000;
     this.feeSyncInterval = config.syncInterval.fee ? config.syncInterval.fee : 3600000;
     this.pendingTxSyncInterval = config.syncInterval.pending ? config.syncInterval.pending : 15000;
   }
 }
 
-module.exports = BtcCrawlerManager;
+module.exports = BchCrawlerManager;
--- src/backend/libs/BchTestnetCrawlerManager.js
+++ src/backend/libs/BchTestnetCrawlerManager.js
@@ -1,13 +1,13 @@
-const BtcCrawlerManagerBase = require('./BtcCrawlerManagerBase');
+const BchCrawlerManagerBase = require('./BchCrawlerManagerBase');
 
-class BtcTestnetCrawlerManager extends BtcCrawlerManagerBase {
+class BchTestnetCrawlerManager extends BchCrawlerManagerBase {
   constructor(config, database, logger) {
     super('80000001', database, logger);
-    this.options = config.blockchain.bitcoin_testnet;
+    this.options = config.blockchain.bitcoin_cash_testnet;
     this.syncInterval = config.syncInterval.bitcoin_cash ? config.syncInterval.bitcoin_cash : 450000;
     this.feeSyncInterval = config.syncInterval.fee ? config.syncInterval.fee : 3600000;
     this.pendingTxSyncInterval = config.syncInterval.pending ? config.syncInterval.pending : 15000;
   }
 }
 
-module.exports = BtcTestnetCrawlerManager;
+module.exports = BchTestnetCrawlerManager;
