39d123464da44fbf0dbcee0effef47e030ca59ec
TzuHanLiang <crocodilestear@gmail.com>
2021-05-27 13:38:25 +0800

    feat: test

--- src/backend/libs/HDWallet.js
+++ src/backend/libs/HDWallet.js
@@ -66,7 +66,7 @@ class HDWallet {
     } else if (blockchainID === '80000091' || blockchainID === 'F0000091') {
       let _node = bitcoin.bip32.fromBase58(
         _serializedExtendPublicKey,
-        bitcoin.networks[coinType === 1 ? 'bitcoin' : 'testnet'],
+        bitcoin.networks[coinType !== 1 ? 'bitcoin' : 'testnet'],
       ); // don't change this
       _node = _node.derive(change).derive(index);
       publicKey = _node.publicKey.toString('hex');
