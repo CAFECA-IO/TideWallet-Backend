15d21ec7e7e7d6ec7b738d13c805714076fab853
wayne.lee <wayne.lee@tideisun.com>
2021-10-22 20:47:58 +0800

    fix getNonce

--- src/backend/libs/Blockchain.js
+++ src/backend/libs/Blockchain.js
@@ -567,7 +567,7 @@ class Blockchain extends Bot {
           option.data = {
             jsonrpc: '2.0',
             method: 'eth_getTransactionCount',
-            params: [address, 'latest'],
+            params: [address, 'pending'],
             id: dvalue.randomID(),
           };
           const data = await Utils.ETHRPC(option);
